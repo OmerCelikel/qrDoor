b09ab8ed93f9eeaafe3d7e05d6e2a0038e939f36
Omer Oguz Celikel <44753206+OmerCelikel@users.noreply.github.com>
2023-05-08 20:58:32 +0300

    Project Created FireBase Added

---
+++ qrDoor/qrDoor/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  qrDoor
+//
+//  Created by Ömer Oğuz Çelikel on 8.05.2023.
+//
+
+import Foundation
+
+struct Constants {
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+    }
+}
