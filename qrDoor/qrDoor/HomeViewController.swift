3339b4d0b2552b704b422639505afbadc5306dc0
Ömer Oğuz <omeroguzcelikel@gmail.com>
2023-05-09 14:02:47 +0300

    User able to add house

--- qrDoor/qrDoor/HomeViewController.swift
+++ qrDoor/qrDoor/HomeViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//        // Get a reference to the current user's document
+//        guard let currentUser = Auth.auth().currentUser else {
+//            // If there is no logged in user, show an error message and return
+//            return
+//        }
+//
+//        let db = Firestore.firestore()
+//        let userRef = db.collection("users").document(currentUser.uid)
+//        let houseData: [String: Any] = [
+//            "address": "123 Main St",
+//            "city": "San Francisco",
+//            "state": "CA",
+//            "owner": userRef
+//        ]
+//
+//        // Add the new house document to the "houses" collection
+//        db.collection("houses").addDocument(data: houseData) { error in
+//            if let error = error {
+//                // Show error message if there was an error adding the new house document
+//                print("Error adding new house: \(error.localizedDescription)")
+//            } else {
+//                // Show success message if the new house document was added successfully
+//                print("Success", "New house added successfully!")
+//            }
+//
+//        }
     }
-
-
 }
-
